493b4fb665e8dd98d8fb54b4e88de0dedfb97fcc
G3 <tuxgal@Baymax.area51>
2024-06-16 18:15:11 -0700

    feat: Set umask to 0002 granting write permissions to the group.

--- scripts/start-bitcoind.sh
+++ scripts/start-bitcoind.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -E -e -o pipefail
 
-# TODO: Make this more extensible to allow configuring
-# the right settings at the time of container startup.
-exec bitcoind -regtest -datadir=/data/bitcoind
+set_umask() {
+    # Configure umask to allow write permissions for the group by default
+    # in addition to the owner.
+    umask 0002
+}
+
+start_bitcoind() {
+    # TODO: Make this more extensible to allow configuring
+    # the right settings at the time of container startup.
+    exec bitcoind -regtest -datadir=/data/bitcoind
+}
+
+set_umask
+start_bitcoind
